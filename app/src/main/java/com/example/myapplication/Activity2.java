11bfed3c582e534f193cb45af43a584651b5deea
zakosadoun <noudasz@gmail.com>
2020-05-05 16:31:20 -0500

    finished

--- app/src/main/java/com/example/myapplication/Activity2.java
+++ app/src/main/java/com/example/myapplication/Activity2.java
@@ -3,6 +3,11 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -10,5 +15,338 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        Button btn = (Button) findViewById(R.id.bl);
+        Button btn1 = (Button) findViewById(R.id.bm);
+        Button btn2 = (Button) findViewById(R.id.br);
+        Button btn3 = (Button) findViewById(R.id.tl);
+        Button btn4 = (Button) findViewById(R.id.tm);
+        Button btn5 = (Button) findViewById(R.id.tr);
+        Button btn6 = (Button) findViewById(R.id.ml);
+        Button btn7 = (Button) findViewById(R.id.mm);
+        Button btn8 = (Button) findViewById(R.id.mr);
+
+
+
+
+        final ImageView goal = (ImageView) findViewById(R.id.goal);
+        final ImageView miss = (ImageView) findViewById(R.id.miss);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Math.random() < .66) {
+                    goal.setVisibility(View.VISIBLE);
+                    Timer t = new Timer(false);
+                    t.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            runOnUiThread(new Runnable() {
+                                public void run() {
+                                    goal.setVisibility(View.INVISIBLE);
+                                }
+                            });
+                        }
+                    }, 2000);
+                } else {
+                    miss.setVisibility(View.VISIBLE);
+                    Timer t = new Timer(false);
+                    t.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            runOnUiThread(new Runnable() {
+                                public void run() {
+                                    miss.setVisibility(View.INVISIBLE);
+                                }
+                            });
+                        }
+                    }, 2000);
+
+
+                }
+
+            }
+        });
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Math.random() < .33) {
+                    goal.setVisibility(View.VISIBLE);
+                    Timer t = new Timer(false);
+                    t.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            runOnUiThread(new Runnable() {
+                                public void run() {
+                                    goal.setVisibility(View.INVISIBLE);
+                                }
+                            });
+                        }
+                    }, 2000);
+                } else {
+                    miss.setVisibility(View.VISIBLE);
+                    Timer t = new Timer(false);
+                    t.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            runOnUiThread(new Runnable() {
+                                public void run() {
+                                    miss.setVisibility(View.INVISIBLE);
+                                }
+                            });
+                        }
+                    }, 2000);
+
+
+                }
+
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Math.random() < .66) {
+                    goal.setVisibility(View.VISIBLE);
+                    Timer t = new Timer(false);
+                    t.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            runOnUiThread(new Runnable() {
+                                public void run() {
+                                    goal.setVisibility(View.INVISIBLE);
+                                }
+                            });
+                        }
+                    }, 2000);
+                } else {
+                    miss.setVisibility(View.VISIBLE);
+                    Timer t = new Timer(false);
+                    t.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            runOnUiThread(new Runnable() {
+                                public void run() {
+                                    miss.setVisibility(View.INVISIBLE);
+                                }
+                            });
+                        }
+                    }, 2000);
+
+
+                }
+
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Math.random() < .75) {
+                    goal.setVisibility(View.VISIBLE);
+                    Timer t = new Timer(false);
+                    t.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            runOnUiThread(new Runnable() {
+                                public void run() {
+                                    goal.setVisibility(View.INVISIBLE);
+                                }
+                            });
+                        }
+                    }, 2000);
+                } else {
+                    miss.setVisibility(View.VISIBLE);
+                    Timer t = new Timer(false);
+                    t.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            runOnUiThread(new Runnable() {
+                                public void run() {
+                                    miss.setVisibility(View.INVISIBLE);
+                                }
+                            });
+                        }
+                    }, 2000);
+
+
+                }
+
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Math.random() < .50) {
+                    goal.setVisibility(View.VISIBLE);
+                    Timer t = new Timer(false);
+                    t.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            runOnUiThread(new Runnable() {
+                                public void run() {
+                                    goal.setVisibility(View.INVISIBLE);
+                                }
+                            });
+                        }
+                    }, 2000);
+                } else {
+                    miss.setVisibility(View.VISIBLE);
+                    Timer t = new Timer(false);
+                    t.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            runOnUiThread(new Runnable() {
+                                public void run() {
+                                    miss.setVisibility(View.INVISIBLE);
+                                }
+                            });
+                        }
+                    }, 2000);
+
+
+                }
+
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Math.random() < .75) {
+                    goal.setVisibility(View.VISIBLE);
+                    Timer t = new Timer(false);
+                    t.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            runOnUiThread(new Runnable() {
+                                public void run() {
+                                    goal.setVisibility(View.INVISIBLE);
+                                }
+                            });
+                        }
+                    }, 2000);
+                } else {
+                    miss.setVisibility(View.VISIBLE);
+                    Timer t = new Timer(false);
+                    t.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            runOnUiThread(new Runnable() {
+                                public void run() {
+                                    miss.setVisibility(View.INVISIBLE);
+                                }
+                            });
+                        }
+                    }, 2000);
+
+
+                }
+
+            }
+        });
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Math.random() < .50) {
+                    goal.setVisibility(View.VISIBLE);
+                    Timer t = new Timer(false);
+                    t.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            runOnUiThread(new Runnable() {
+                                public void run() {
+                                    goal.setVisibility(View.INVISIBLE);
+                                }
+                            });
+                        }
+                    }, 2000);
+                } else {
+                    miss.setVisibility(View.VISIBLE);
+                    Timer t = new Timer(false);
+                    t.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            runOnUiThread(new Runnable() {
+                                public void run() {
+                                    miss.setVisibility(View.INVISIBLE);
+                                }
+                            });
+                        }
+                    }, 2000);
+
+
+                }
+
+            }
+        });
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Math.random() < .25) {
+                    goal.setVisibility(View.VISIBLE);
+                    Timer t = new Timer(false);
+                    t.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            runOnUiThread(new Runnable() {
+                                public void run() {
+                                    goal.setVisibility(View.INVISIBLE);
+                                }
+                            });
+                        }
+                    }, 2000);
+                } else {
+                    miss.setVisibility(View.VISIBLE);
+                    Timer t = new Timer(false);
+                    t.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            runOnUiThread(new Runnable() {
+                                public void run() {
+                                    miss.setVisibility(View.INVISIBLE);
+                                }
+                            });
+                        }
+                    }, 2000);
+
+
+                }
+
+            }
+        });
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Math.random() < .50) {
+                    goal.setVisibility(View.VISIBLE);
+                    Timer t = new Timer(false);
+                    t.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            runOnUiThread(new Runnable() {
+                                public void run() {
+                                    goal.setVisibility(View.INVISIBLE);
+                                }
+                            });
+                        }
+                    }, 2000);
+                } else {
+                    miss.setVisibility(View.VISIBLE);
+                    Timer t = new Timer(false);
+                    t.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            runOnUiThread(new Runnable() {
+                                public void run() {
+                                    miss.setVisibility(View.INVISIBLE);
+                                }
+                            });
+                        }
+                    }, 2000);
+
+
+                }
+
+            }
+        });
     }
+
+
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,24 +4,81 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
 public class MainActivity extends AppCompatActivity {
+    SignInButton signin;
+    GoogleSignInClient mGoogleSignInClient;
+    int RC_SIGN_IN = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn = (Button) findViewById(R.id.startbtn);
-        btn.setOnClickListener(new View.OnClickListener() {
+
+
+        signin = findViewById(R.id.sign_in_button);
+        signin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openActivity2();
+                switch (view.getId()) {
+                    case R.id.sign_in_button:
+                        signIn();
+                        break;
+                    // ...
+                }
 
             }
         });
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
     }
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            Intent intent = new Intent (MainActivity.this, Activity2.class);
+            startActivity(intent);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("Error", "signInResult:failed code=" + e.getStatusCode());
+        }
+    }
+
+
     public void openActivity2() {
         Intent intent = new Intent(this, Activity2.class);
         startActivity(intent);
